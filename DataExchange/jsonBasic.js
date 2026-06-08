207d5f8e0dea2da23a2855bd81590eccea2b604e
Sami Thakur <2022.sami.thakur@ves.ac.in>
2026-06-08 23:58:33 +0530

    JSON and XML

---
+++ DataExchange/jsonBasic.js
@@ -0,0 +1,34 @@
+// JavaScript Object Notation (lightweight format used to exchange data between systems)
+
+const JSON = {
+    // Primitive Data Types
+    "id": 101,                 // Number
+    "name": "Sami",            // String
+    "age": 21,                 // Number
+    "isStudent": true,         // Boolean
+    // Array (Collection of values)
+    "skills": [
+        "Java",
+        "Python",
+        "SQL"
+    ],
+    // Nested Object
+    "address": {
+        "city": "Mumbai",
+        "state": "Maharashtra",
+        "country": "India"
+    },
+    // Array of Objects
+    "projects": [
+        {
+            "projectId": 1,
+            "projectName": "Smart Education"
+        },
+        {
+            "projectId": 2,
+            "projectName": "AI Traffic System"
+        }
+    ],
+    // Null Value
+    "phoneNumber": null
+}
